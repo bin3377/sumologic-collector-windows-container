b895b2c9d4f4ccaddd84d5aabccb8f580aba0e27
Bin Yi <byi@sumologic.com>
2019-03-18 11:45:58 -0700

    update build scripts

--- make.bat
+++ make.bat
@@ -1,2 +1,4 @@
 @echo off
 docker build --no-cache=true -f ./Dockerfile -t sumologic/collector-win-beta .
+
+REM docker run -it -u ContainerAdministrator -v //./pipe/docker_engine://./pipe/docker_engine sumologic/collector-win-beta
